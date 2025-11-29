2d4172347d5a944467b377336eead54b6cf21512
Brandon Chung <jbx.chung@gmail.com>
2025-11-28 21:08:12 -0500

    importer execution should be POST

--- src/routes/importer.route.ts
+++ src/routes/importer.route.ts
@@ -10,6 +10,6 @@ router.get('/', handle(importerController.getImporters));
 router.post('/', handle(importerController.createImporter));
 router.patch('/:id', handle(importerController.updateImporter));
 router.delete('/:id', handle(importerController.deleteImporter));
-router.get('/:id/execute', upload.single('inputFile'), handle(importerController.executeImporter));
+router.post('/:id/execute', upload.single('inputFile'), handle(importerController.executeImporter));
 
 export default router;
