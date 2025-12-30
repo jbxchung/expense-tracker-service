bbcd538ac1e9f07f96cc797020fe5eca6aaa84fa
Brandon Chung <jbx.chung@gmail.com>
2025-12-30 17:08:29 -0500

    fix signup validation

--- src/utils/password.util.ts
+++ src/utils/password.util.ts
@@ -2,12 +2,12 @@ export function validatePassword(password: string | undefined): boolean {
   if (!password) return false;
 
   const minLength = 8;
-  const hasLetter = /[a-zA-Z]/.test(password);
+  const hasUpperAndLower = /[a-z]/.test(password) && /[A-Z]/.test(password);
   const hasNumber = /\d/.test(password);
 
   return (
     password.length >= minLength &&
-    hasLetter &&
+    hasUpperAndLower &&
     hasNumber
   );
 }
